90ee94bd4ec1c8b922de942cb18659e30a24c125
Said <zitouni.sd@gmail.com>
2021-07-23 21:13:08 +0100

    setup database seeder and create fake data, interesting snippet in seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use App\Models\Listing;
+use App\Models\User;
+use App\Models\Tag;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -13,6 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        //  \App\Models\User::factory(10)->create();
+        $tags = Tag::factory(10)->create();
+        // for listings there is a foreign key so we need to cascade using user_id
+        User::factory(10)->create()->each(function($user) use($tags){
+            Listing::factory(rand(1,4))->create([
+                'user_id' => $user->id
+            ])->each(function($listing) use($tags){
+                $listing->tags()->attach($tags->random(2));
+            });
+        });
     }
 }
