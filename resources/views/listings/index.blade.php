1c80e15c3763cb121bb4a7a17465190090dcb286
Said <zitouni.sd@gmail.com>
2021-07-28 18:53:31 +0100

    continue landing page for listings

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -7,17 +7,30 @@
                 {{$tag->name}}</a>
             @endforeach
             <div class="mb-12">
-                <h2 class="text-2xl font-medium text-gray-900 title-font px-4"> All jobs ({{$listings->count()}})</h2>
+                <h2 class="px-4 text-2xl font-medium text-gray-900 title-font"> All jobs ({{$listings->count()}})</h2>
             </div>
             <div class="my-6">
                 @foreach ($listings as $listing )
                     <a href="#"
                     class="py-6 px-4 flex flex-wrap md:flex-nowrap border-b border-gray-100 {{$listing->is_highlighted ? 'bg-yellow-100 hover:bg-yellow-200' : 'bg-white hover:bg-gray-100'}}"
                     >
-                    <span class="md:w-16 md:mb-10"></span>
+                    <div class="flex flex-col flex-shrink-0 mb-6 mr-4 md:w-16 md:mb-10">
+                        <img class="object-cover w-16 h-16 rounded-full" src="/storage/{{ $listing->logo }}" alt="{{ $listing->company }}">
+                    </div>
+                    <h2 class="mb-1 text-xl font-bold text-gray-900 title-font"></h2>
                     </a>
-                @endforeach
+                    <p class="leading-relaxed text-gray-900">
+                        {{ $listing->company }} &mdash; <span class="text-gray-600">{{ $listing->location }}</span>
+                    </p>
             </div>
+                <div class="flex items-center justify-start mr-8 md:flex-grow">
+
+                    @foreach ($listing->tags as $tag)
+                        
+                    @endforeach
+                    
+                </div>
+                @endforeach
         </div>
     </section>
 </x-app-layout>
