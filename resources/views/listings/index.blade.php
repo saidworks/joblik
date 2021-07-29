18d83985e041c368a6afcff5a1853b9c4873c947
Said ZITOUNI <zitouni.sd@gmail.com>
2021-07-29 08:01:59 +0100

    landing page complete need to work on search bar

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -17,20 +17,28 @@
                     <div class="flex flex-col flex-shrink-0 mb-6 mr-4 md:w-16 md:mb-10">
                         <img class="object-cover w-16 h-16 rounded-full" src="/storage/{{ $listing->logo }}" alt="{{ $listing->company }}">
                     </div>
-                    <h2 class="mb-1 text-xl font-bold text-gray-900 title-font"></h2>
-                    </a>
-                    <p class="leading-relaxed text-gray-900">
-                        {{ $listing->company }} &mdash; <span class="text-gray-600">{{ $listing->location }}</span>
-                    </p>
-            </div>
-                <div class="flex items-center justify-start mr-8 md:flex-grow">
+                    <div class="md:w-1/2 mr-8 flex flex-col items-start justify-center">
+                        <h2 class="mb-1 text-xl font-bold text-gray-900 title-font"> {{$listing->title}}</h2>
 
-                    @foreach ($listing->tags as $tag)
-                        
-                    @endforeach
-                    
-                </div>
+                        <p class="leading-relaxed text-gray-900">
+                            {{ $listing->company }} &mdash; <span class="text-gray-600">{{ $listing->location }}</span>
+                        </p>
+                    </div>
+                    <div class="flex items-center justify-start mr-8 md:flex-grow">
+
+                        @foreach ($listing->tags as $tag)
+                            <span class="inline-block ml-2 tracking-wide text-xs font-medium title-font py-0.5 px-1.5 border border-indigo-500 uppercase {{$tag->slug == request()->get('tag') ? 'bg-indigo-500 text-white' :'bg-white text-indigo-500'}}">
+                            {{$tag->name}}
+                            </span>
+                            <span class="md:flex-grow flex items-center justify-end">
+                                <span> {{$listing->created_at->diffForHumans() }}</span>
+                            </span>
+                        @endforeach
+
+                    </div>
+                </a>
                 @endforeach
+            </div>
         </div>
     </section>
 </x-app-layout>
